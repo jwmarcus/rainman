81f0150df56d11cf3da7a266f9fea2dba0a3cabe
John Marcus III <j3@jwmarcus.com>
2024-03-10 21:05:46 -0400

    figured out pydantic errors

--- app/models.py
+++ app/models.py
@@ -1,19 +1,21 @@
 from typing import Optional, Any
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import BaseModel, validator
+from datetime import datetime, timezone
 
-import json
-import requests
+
+# TODO: Encode this into an enum or something
+# https://en.wikipedia.org/wiki/METAR#METAR_weather_codes
 
 
 class Cloud(BaseModel):
     """
-    Represents a cloud object. Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
+    Represents a cloud object.
+    Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
 
     Attributes:
-        cover (str): The cloud cover type.
-        base (int): The cloud base height.
-        type (Optional[str]): The cloud type (optional). Only value is "CB"
+        cover (str): The cover of the cloud.
+        base (int): The base of the cloud.
+        type (Optional[str]): The type of the cloud (optional).
     """
 
     cover: str
@@ -23,29 +25,30 @@ class Cloud(BaseModel):
 
 class Forecast(BaseModel):
     """
-    Represents a weather forecast. Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
+    Represents a weather forecast.
+    Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
 
     Attributes:
         timeGroup (int): The time group of the forecast.
-        timeFrom (int): The starting time of the forecast. In UNIX Epoch format
-        timeTo (int): The ending time of the forecast.
-        timeBec (Optional[Any]): The time when the forecast becomes valid.
+        timeFrom (datetime): The starting time of the forecast.
+        timeTo (datetime): The ending time of the forecast.
+        timeBec (Optional[datetime]): The time when the forecast becomes valid.
         fcstChange (Optional[str]): The forecast change information.
-        probability (Optional[Any]): The probability of the forecast.
+        probability (Optional[str]): The probability of the forecast.
         wdir (int): The wind direction.
         wspd (int): The wind speed.
         wgst (Optional[int]): The wind gust speed.
-        wshearHgt (Optional[Any]): The height of wind shear.
-        wshearDir (Optional[Any]): The direction of wind shear.
-        wshearSpd (Optional[Any]): The speed of wind shear.
-        visib (str): The visibility.
-        altim (Optional[Any]): The altimeter setting.
-        vertVis (Optional[Any]): The vertical visibility.
-        wxString (Optional[Any]): The weather string.
-        notDecoded (Optional[Any]): The not decoded information.
-        clouds (List[Cloud]): The list of cloud information.
-        icgTurb (List[Any]): The list of icing and turbulence information.
-        temp (List[Any]): The list of temperature information.
+        wshearHgt (Optional[int]): The wind shear height.
+        wshearDir (Optional[int]): The wind shear direction.
+        wshearSpd (Optional[int]): The wind shear speed.
+        visib (int | str): The visibility.
+        altim (Optional[str]): The altimeter setting.
+        vertVis (Optional[str]): The vertical visibility.
+        wxString (Optional[str]): The weather string.
+        notDecoded (Optional[str]): The not decoded information.
+        clouds (list[Cloud]): The list of cloud information.
+        icgTurb (list[str]): The list of icing and turbulence information.
+        temp (list[str]): The list of temperature information.
     """
 
     timeGroup: int
@@ -70,29 +73,28 @@ class Forecast(BaseModel):
     temp: list[str]
 
 
-
 class TAF(BaseModel):
     """
-    Represents a Terminal Aerodrome Forecast (TAF).  Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
+    Represents a Terminal Aerodrome Forecast (TAF) object.
+    Based on https://aviationweather.gov/data/api/#/Data/dataTaf.
 
     Attributes:
-        tafId (int): The TAF ID.
-        icaoId (str): The ICAO ID.
-        dbPopTime (str): The database population time.
-        bulletinTime (str): The bulletin time.
-        issueTime (str): The issue time.
-        validTimeFrom (int): The valid time from.
-        validTimeTo (int): The valid time to.
-        rawTAF (str): The raw TAF data.
-        mostRecent (int): The most recent flag.
-        remarks (str): The remarks.
-        lat (float): The latitude.
-        lon (float): The longitude.
-        elev (int): The elevation.
-        prior (int): The prior flag.
-        name (str): The name of the location.
-        fcsts (List[Forecast]): The list of forecasts.
-
+        tafId (int): The unique identifier for the TAF.
+        icaoId (str): The ICAO code for the location associated with the TAF.
+        dbPopTime (datetime): The database population time for the TAF.
+        bulletinTime (datetime): The bulletin time for the TAF.
+        issueTime (datetime): The issue time for the TAF.
+        validTimeFrom (datetime): The start time of the validity period for the TAF.
+        validTimeTo (datetime): The end time of the validity period for the TAF.
+        rawTAF (str): The raw text of the TAF.
+        mostRecent (int): Indicator for the most recent TAF.
+        remarks (str): Additional remarks for the TAF.
+        lat (float): The latitude of the location associated with the TAF.
+        lon (float): The longitude of the location associated with the TAF.
+        elev (int): The elevation of the location associated with the TAF.
+        prior (int): Indicator for a prior TAF.
+        name (str): The name of the location associated with the TAF.
+        fcsts (list[Forecast]): A list of forecast objects associated with the TAF.
     """
 
     tafId: int
@@ -112,16 +114,22 @@ class TAF(BaseModel):
     name: str
     fcsts: list[Forecast] = []
 
+    @validator("dbPopTime", "bulletinTime", "issueTime", pre=True)
+    def convert_time(cls, v):
+        """
+        Convert a string representation of a datetime to an aware datetime object.
+        The avaiationweather.gov API returns datetime objects as strings in the format "YYYY-MM-DD HH:MM:SS".
+        This means we need to set it as an aware datetime object with the timezone set to UTC.
 
-def main():
-    url = "https://aviationweather.gov/api/data/taf?ids=KBOS&format=json&date=2024-03-09T020:00:00Z"
-    response = requests.get(url)
-    data = response.json()
+        Args:
+            v (str): The string representation of the datetime (lacking an offset).
 
-    for taf in data:
-        model = TAF(**taf)
-        print(model.rawTAF)
+        Returns:
+            datetime: The aware datetime object.
 
-
-if __name__ == "__main__":
-    main()
+        Raises:
+            ValueError: If the string representation is not in the expected format.
+        """
+        unaware_time = datetime.strptime(v, "%Y-%m-%d %H:%M:%S")
+        aware_time = unaware_time.replace(tzinfo=timezone.utc)
+        return aware_time
